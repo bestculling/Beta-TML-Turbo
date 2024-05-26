7c56433ce84a3320e9ed0948fae5fa024bfceb45
Akkrapol Kanpong <bestculling@bestculling.local>
2024-05-26 10:23:17 +0700

    allowedOrigins update

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://tml1-turbo.netlify.app', 'http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
